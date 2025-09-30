830a11ab8c596037fbf1a28332bf87768f402dc5
Kakasp96 <ricardosilvate96@gmail.com>
2025-09-29 23:29:06 -0300

    Configurado o YUP porem é nescessario migração para typescript, deixei comentado o YUP.

--- src/app.js
+++ src/app.js
@@ -1,9 +1,29 @@
 
 import express from "express";
 import pool from "./config/dbConnect.js";
+// import { object, string, InferType } from "yup";
 
 const app = express();
 app.use(express.json()); //middleware - Acesso as req e res e fazer algumas ações
+
+// let agendaCreateSchema = object({
+//     scope: string().required("O campo scope é obrigatório."),
+//     time: string().required("O campo time é obrigatório."),
+//     date: string().required("O campo date é obrigatório."),
+//     duration: string().required("O campo duration é obrigatório."),
+//     location: string().required("O campo location é obrigatório.")
+// });
+
+// type AgendaCreate = InferType<typeof agendaCreateSchema>;
+
+// let agendaAlterSchema = object({
+//     scope: string(),
+//     time: string(),
+//     date: string(),
+//     duration: string(),
+//     location: string()
+// });
+// type AgendaAlter = InferType<typeof agendaCreateSchema>;
 
 // const agendas = [
 //     {
@@ -94,11 +114,13 @@ app.get("/agendas/:id", async (req, res) => {
 
 app.post("/agendas", async (req, res) => {
     try {
+        // const validatedData: AgendaCreate = await agendaCreateSchema.validate(req.body, { abortEarly: false });
+
         const{scope,time,date,duration,location} = req.body;
 
         const result = await pool.query(
             'INSERT INTO agendas (scope,time,date,duration,location) VALUES ($1, $2, $3, $4, $5) RETURNING *',
-            [scope, time, date, duration, location]
+            [scope,time,date,duration,location]
         );
 
         res.status(201).json(result.rows[0]);
